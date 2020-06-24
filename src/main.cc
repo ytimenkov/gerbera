8aaf060f068a505cc10c12c99f7561779b731baa
Yuri Timenkov <yuri@timenkov.ru>
2020-06-24 09:55:30 +0000

    touch

--- src/main.cc
+++ src/main.cc
@@ -22,9 +22,7 @@
     
     You should have received a copy of the GNU General Public License
     version 2 along with MediaTomb; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-    
-    $Id$
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.  
 */
 
 /// \file main.cc
